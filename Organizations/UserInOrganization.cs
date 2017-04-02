f745805db1d42ef2b85147c3026ef4e74ae22422
spetz <spetz90@gmail.com>
2017-04-02 13:11:35 +0200

    User in org name

--- Organizations/UserInOrganization.cs
+++ Organizations/UserInOrganization.cs
@@ -6,6 +6,7 @@ namespace Warden.Services.Storage.Models.Organizations
     {
         public string UserId { get; set; }
         public string Email { get; set; }
+        public string Name { get; set; }
         public string Role { get; set; }
         public DateTime CreatedAt { get; set; }
     }
